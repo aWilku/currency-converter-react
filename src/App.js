8ac284472ad4c210b8219c76207f47f09d8092e4
Adrian Wilk <wilku5991@wp.pl>
2021-06-12 13:34:42 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="header">
+        <h1 className="header__title">Wolf Kalkulator</h1>
       </header>
-    </div>
+
+      <form className="form">
+        <fieldset className="form__fieldset">
+          <p>
+            <label>
+              <span className="form__labelText">
+                Kwota w PLN:
+                    </span>
+              <input className="form__field" name="amount" type="number" min="0.01" max="999999999"
+                step="0.01" placeholder="Wpisz kwotę" required />
+            </label>
+          </p>
+          <p>
+            <label>
+              <span className="form__labelText">
+                Waluta:
+                    </span>
+              <select className="form__field" name="requestedCurrency" required>
+                <option value="" disabled selected>Wybierz walutę</option>
+                <option value="EUR">EUR</option>
+                <option value="CHF">CHF</option>
+                <option value="USD">USD</option>
+                <option value="GBP">GBP</option>
+              </select>
+            </label>
+          </p>
+        </fieldset>
+
+        <p>
+          <button className="form__button">Przelicz</button>
+        </p>
+
+        <fieldset className="form__fieldset form__fieldset--special">
+          <legend className="form__legend">Otrzymasz:</legend>
+          <p className="form__paragraph"><strong></strong></p>
+        </fieldset>
+
+        <p>
+          <button className="form__button" type="reset">Wyczyść kalkulator</button>
+        </p>
+      </form>
+    </main>
   );
 }
 
