4c3d35e74278830f0868cf0eee5a0d011a1c0073
Adrian Wilk <wilku5991@wp.pl>
2021-07-12 20:49:33 +0200

    add currenies.js module

---
+++ src/currencies.js
@@ -0,0 +1,24 @@
+const currencies = [
+    {
+        name: "Euro",
+        shortcut: "EUR",
+        rate: 4.56,
+    },
+    {
+        name: "Frank szwajcarski",
+        shortcut: "CHF",
+        rate: 4.19,
+    },
+    {
+        name: "Dolar amerykański",
+        shortcut: "USD",
+        rate: 3.84,
+    },
+    {
+        name: "Funt brytyjski",
+        shortcut: "GBP",
+        rate: 5.32,
+    },
+];
+
+export { currencies };
