772f6568a1ed20a4f4a3db24af6251179cc60a9e
Adrian Wilk <wilku5991@wp.pl>
2021-07-26 17:59:11 +0200

    add styled component to Header

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -1,13 +1,15 @@
-.header {
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
     background-color: #254870;
     border-radius: 5px;
     text-align: center;
     margin: 10px auto;
     width: 95%;
     max-width: 500px;
-}
+`;
 
-.header__title {
+export const StyledTitle = styled.h1`
     color: #ffffff;
     margin: 5px;
-}
+`;
