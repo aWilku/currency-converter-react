6154ead66208d7e5de4349badca5a83cd996db2a
Adrian Wilk <wilku5991@wp.pl>
2021-07-27 22:30:28 +0200

    add flex

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -13,11 +13,15 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: inherit;
     }
 
-    body {
+    #root {
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    min-height:100vh;
+    font-family: 'Lato', sans-serif; 
     background-image: url("${background}");
     background-color: ${({ theme }) => theme.colors.gallery};
     background-size: cover;
     background-attachment: fixed;
-    font-family: 'Lato', sans-serif; 
     }
 `;
