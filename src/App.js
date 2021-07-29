fcc5a69d55ac53f86a2f11b61a0b0b38071551c8
Adrian Wilk <wilku5991@wp.pl>
2021-07-29 22:30:48 +0200

    modify component Container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./Header";
 import Form from "./Form";
-import Container from "./Container";
+import Container from "./Container/styled";
 import { useState } from "react";
 import { currencies } from "./currencies";
 
--- src/Container/styled.js
+++ src/Container/styled.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
-export const StyledContainer = styled.main`
+const Container = styled.main`
     max-width: 1024px;
     width: 100%;
     margin: auto;
 `;
+
+export default Container;
